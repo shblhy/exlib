8d7e6a191f541f362c37dd4f5fdb9f46c37b1945
yan.huang <122-yan.huang@users.noreply.gitlab.hmswork.space>
2022-03-16 12:03:24 +0800

    feat:config支持动态property

--- config/apollo.py
+++ config/apollo.py
@@ -1,19 +1,60 @@
+import json
+import types
 from .base import Config
 from ..libs.apollo_client import ApolloClient
-from werkzeug.local import LocalProxy
+
+
+def get_value(name, format='str', value=None):
+    """
+        返回yige property,动态从apollo获取值
+    :param name:
+    :param format:
+    :param value: 默认值
+    :return:
+    """
+    def get(self):
+        r = self.client.get_value(name, namespace=self.namespace)
+        parsers = {
+            'json': lambda x: json.loads(x) if x else None,
+            'int': lambda x: int(x) if x else None,
+            'float': lambda x: int(x) if x else None
+        }
+        if format in parsers:
+            return parsers[format](r)
+        return r if r is not None else value
+    return property(get)
 
 
 class ApolloConfig(Config):
-    def _get_value(self, name, value=None):
-        r = self.client.get_value(name)
+    def _get_value(self, name, format='str', value=None):
+        r = self.client.get_value(name, namespace=self.namespace)
+        parsers = {
+            'json': lambda x: json.loads(x) if x else None,
+            'int': lambda x: int(x) if x else None,
+            'float': lambda x: int(x) if x else None
+        }
+        if format in parsers:
+            return parsers[format](r)
         return r if r is not value else value
 
     def __init__(self, app_id, server, auth):
+        self.namespace = 'quant.dev'
         self.client = ApolloClient(app_id=app_id,
                                    config_server_url=server,
                                    authorization=auth)
+        self.set_dynamic_property()
+
+    def set_dynamic_property(self):
+        """
+            动态绑定property，以确保属性是从配置中心获取最新而非缓存。
+        :return:
+        """
         for key in self.apollo_keys:
-            setattr(self, key, LocalProxy(lambda : self._get_value(key)))
+            if '__' in key:
+                key, format = key.split('__')
+            else:
+                format = 'str'
+            setattr(ApolloConfig, key, get_value(key, format))
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.client.stopped = True
