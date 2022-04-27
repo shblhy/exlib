2c6d0fbf1efb9822ff32a18fa4ab0fccf8783bb3
yan.huang <122-yan.huang@users.noreply.gitlab.hmswork.space>
2022-04-27 20:37:16 +0800

    配置中心升级,支持自定义engines

--- config/apollo.py
+++ config/apollo.py
@@ -1,69 +1,16 @@
-import json
-import types
-from .base import Config
+from .base import ConfigEngine, parse
 from ..libs.apollo_client import ApolloClient
 
 
-def get_value(name, format='str', value=None):
-    """
-        返回yige property,动态从apollo获取值
-    :param name:
-    :param format:
-    :param value: 默认值
-    :return:
-    """
-    def get(self):
-        r = self.client.get_value(name, namespace=self.namespace)
-        parsers = {
-            'json': lambda x: json.loads(x) if x else None,
-            'int': lambda x: int(x) if x else None,
-            'float': lambda x: int(x) if x else None
-        }
-        if format in parsers:
-            return parsers[format](r)
-        return r if r is not None else value
-    return property(get)
+class ApolloEngine(ConfigEngine):
+    target = 'apollo_keys'
 
+    def init(self, conf):
+        self._conf_ = conf
+        self.client = ApolloClient(app_id=conf.apollo_app_id,
+                                   config_server_url=conf.apollo_app_server,
+                                   authorization=conf.apollo_app_auth)
+        self.namespace = conf.apollo_namespace or 'application'
 
-class ApolloConfig(Config):
-    apollo_keys = []    # 设置在阿波罗的key必须进行登记
-
-    def _get_value(self, name, format='str', value=None):
-        r = self.client.get_value(name, namespace=self.namespace)
-        parsers = {
-            'json': lambda x: json.loads(x) if x else None,
-            'int': lambda x: int(x) if x else None,
-            'float': lambda x: int(x) if x else None
-        }
-        if format in parsers:
-            return parsers[format](r)
-        return r if r is not value else value
-
-    def __init__(self, app_id, server, auth=None, namespace='application'):
-        self.namespace = namespace
-        self.client = ApolloClient(app_id=app_id,
-                                   config_server_url=server,
-                                   authorization=auth)
-        self.set_dynamic_property()
-
-    def set_dynamic_property(self):
-        """
-            动态绑定property，以确保属性是从配置中心获取最新而非缓存。
-        :return:
-        """
-        for key in self.apollo_keys:
-            if '__' in key:
-                key, format = key.split('__')
-            else:
-                format = 'str'
-            setattr(ApolloConfig, key, get_value(key, format))
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.client.stopped = True
-
-    @property
-    def apollo_attrs(self):
-        return [k.split('__')[0] if '__' in k else k for k in self.apollo_keys]
-
-    def get_apollo_data(self):
-        return {k: getattr(self, k) for k in self.apollo_attrs}
+    def _get_value(self, name):
+        return self.client.get_value(name, namespace=self.namespace)
--- config/base.py
+++ config/base.py
@@ -1,9 +1,100 @@
+import json
+import logging
+
+VERSION = '0.11'
+logger = logging.getLogger('default')
+
+
 class Config:
-    debug = False
-    run_local = False
+    """
+        config为一份配置，可能由来自多处的数据组成，各处的数据由各自的"引擎"engine提取。
+        例如 代码上、环境变量上、类似阿波罗的配置中心里、外部配置接口获取。
+        编写时，需要初始化引擎，设置属性的获取方式。
+        对用户，就是 配置类.属性 的使用方式。无需关注属性是普通字段还是property方法。
+        注意，engines按顺序执行，按顺序初始化各个字段。
+    """
+    _engines = []
+    _engines_ = {}
+
+    def __init__(self):
+        self.setup_engine()
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_instance"):
             if not hasattr(cls, "_instance"):
                 cls._instance = object.__new__(cls)
         return cls._instance
+
+    def setup_engine(self):
+        for engine in self._engines:
+            e = engine()
+            e.init(self)    # 初始化引擎
+            e.setup(self)
+            self._engines_[engine.__name__] = e
+
+    def get_all_property_keys(self):
+        keys = []
+        for engine in self._engines:
+            keys.extend(getattr(self, engine.target))
+        return keys
+
+    def get_all_keys(self):
+        return self.get_all_property_keys()
+
+    def get_ori_keys(self, keys):
+        return [k.split('__')[0] if '__' in k else k for k in keys]
+
+    def get_data(self, keys=None):
+        keys = keys or self.get_all_keys()
+        return {k: getattr(self, k) for k in self.get_ori_keys(keys)}
+
+
+def parse(r, format, value):
+    parsers = {
+        'json': lambda x: json.loads(x) if x else None,
+        'int': lambda x: int(x) if x else None,
+        'float': lambda x: int(x) if x else None
+    }
+    if format in parsers:
+        return parsers[format](r)
+    return r if r is not value else value
+
+
+def get_property(name, format='str', value=None, engine=None):
+    """
+        返回一个 property,动态获取值，如果无法取得值，则提供上一次的数据。
+    """
+    def get(self):
+        try:
+            r = engine._get_value(name)
+            self.__dict__[name] = parse(r, format, value)
+        except Exception as e:
+            logger.exception(e)
+        return self.__dict__[name]
+    return property(get)
+
+
+class ConfigEngine:
+    def init(self, conf):
+        """
+            初始动作，如连接服务、确认文件等。
+        :param conf:
+        :return:
+        """
+        pass
+
+    def setup(self, config):
+        """
+            为config设置property
+        """
+        if not hasattr(config, self.target):
+            return
+        keys = getattr(config, self.target)
+        if not keys:
+            return
+        for key in keys:
+            if '__' in key:
+                key, format = key.split('__')
+            else:
+                format = 'str'
+            setattr(config.__class__, key, get_property(key, format, engine=self))
