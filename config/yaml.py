872c8f30f90ba59e7f5bc882d629ae4eaf38079d
yan.huang <122-yan.huang@users.noreply.gitlab.hmswork.space>
2022-07-15 18:43:07 +0800

    yaml配置实时更新

--- config/yaml.py
+++ config/yaml.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 from exlib.config.base import ConfigEngine
 
 
@@ -12,10 +13,18 @@ class YamlEngine(ConfigEngine):
         self.target = target
 
     def init(self, conf):
-        self._yml_data_ = {}
-        yaml_path = getattr(conf, self.from_file)
-        with open(yaml_path) as file:
-            self._yml_data_ = yaml.safe_load(file)
+        self.yaml_path = getattr(conf, self.from_file)
+        self._last_yml_time_ = None
+
+    @property
+    def _yml_data(self):
+        last_yml_time = os.path.getmtime(self.yaml_path)
+        if not hasattr(self, '_yml_data_') or last_yml_time != self._last_yml_time_:
+            self._yml_data_ = {}
+            with open(self.yaml_path) as file:
+                self._yml_data_ = yaml.safe_load(file)
+            self._last_yml_time_ = os.path.getmtime(self.yaml_path)
+        return self._yml_data_
 
     def _get_value(self, name):
-        return self._yml_data_.get(name)
+        return self._yml_data.get(name)
