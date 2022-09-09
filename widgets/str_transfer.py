7973317c479a2326d8f468f621236b7532ec8912
yan.huang <122-yan.huang@users.noreply.gitlab.hmswork.space>
2022-09-09 12:00:02 +0800

    增加transfer_hump

--- widgets/str_transfer.py
+++ widgets/str_transfer.py
@@ -10,3 +10,14 @@ def transfer_underline(name):
     while res.endswith('_'):
         res = res[-1:]
     return res
+
+
+def transfer_hump(name, ignore_first=True):
+    """下划线转驼峰"""
+    res = []
+    for index, n in enumerate(name.split('_')):
+        if ignore_first and index == 0:
+            res.append(n)
+        else:
+            res.append(f'{n[0].upper()}{n[1:]:}')
+    return ''.join(res)
